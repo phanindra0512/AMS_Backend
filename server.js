3f396ba0c5494a9b8f9987ddcd6cbd437078fb19
Phanindra98 <Sai.s.Phanindra@98thpercentile.com>
2026-01-22 22:59:40 +0530

    updated changes in verify otp

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config();
 const { swaggerUi, swaggerSpec } = require("./swagger");
 
 const app = express();
